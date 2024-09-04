34258ad5874e26aa57b1787f79d95898ee968085
adanSmith01 <smith.paredes.13@gmail.com>
2024-09-04 17:01:12 -0300

    Directorio de pruebas

---
+++ tests/TestTienda.cs
@@ -0,0 +1,21 @@
+﻿namespace tests;
+using Sistema_gestion;
+using Xunit;
+
+public class TestTienda
+{
+    [Fact]
+
+    public void AgregarProductoNoExistente()
+    {
+        var tienda = new Tienda();
+        var producto = new Producto{Nombre = "prod-1", Precio=200, Categoria="cat-1"};
+        var cant_ant = tienda.Inventario.Count;
+
+        tienda.AgregarProducto(producto);
+
+        var cant_act = tienda.Inventario.Count;
+
+        Assert.Equal<int>(cant_ant+1, cant_act);
+    }
+}
