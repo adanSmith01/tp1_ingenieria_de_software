9ad88136110b70f82e7b64eabe79ee573a6ba601
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2024-09-06 20:59:28 -0300

    ejemplo de mook

--- sistema_tienda/Tienda.cs
+++ sistema_tienda/Tienda.cs
@@ -1,12 +1,19 @@
 namespace Sistema_gestion;
 
+public interface IProductoRepositorio
+{
+    Producto BuscarProducto(string nombre);
+    void ActualizarProducto(Producto producto);
+}
 public class Tienda
 {
     public List<Producto> Inventario;
+    private readonly IProductoRepositorio _productRepositorio;
 
-    public Tienda()
+    public Tienda(IProductoRepositorio productRepositorio)
     {
         Inventario = new List<Producto>();
+        _productRepositorio = productRepositorio;
     }
 
     public void AgregarProducto(Producto p)
@@ -49,6 +56,16 @@ public class Tienda
         }
 
         throw new Exception("Error al eliminar el producto");
+    }
+
+    public void AplicarDescuento(string nombre, double porcentaje)
+    {
+        Producto producto = _productRepositorio.BuscarProducto(nombre);
+        if (producto != null)
+        {
+            producto.Precio -= producto.Precio * (porcentaje / 100);
+            _productRepositorio.ActualizarProducto(producto);
+        }
     }
 
 }
